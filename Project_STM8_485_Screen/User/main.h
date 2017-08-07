5f49c890dfb0c671edad064b488bbd8b954966ad
hgyy <953711266@qq.com>
2017-08-07 17:25:22 +0800

    完成按键控制，LED闪烁部分的硬件功能程序编写

---
+++ Project_STM8_485_Screen/User/main.h
@@ -0,0 +1,63 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+//-----头文件----
+#include "stm8l15x.h"
+#include "stm8l15x_it.h"
+#include "stm8l15x_pwr.h"
+#include <stdio.h>
+
+
+#include "HAL_usart.h"
+#include "HAL_gpio.h"
+#include "HAL_tim.h"
+#include "HAL_key.h"
+#include "HAL_led.h"
+#include "HAL_eeprom.h"
+
+#include "APP_nor_mode.h"
+#include "APP_spider.h"
+#include "APP_iwdg.h"
+#include "APP_Camera.h"
+#include "APP_flash.h"
+#include "APP_led.h"
+
+#include "sysclk.h"
+
+#include "FUN_algorithm.h"
+#include "PRO_spider.h"
+
+//--ST库头文件--
+#include "stm8l15x_gpio.h"
+#include "stm8l15x_usart.h"
+#include "stm8l15x_spi.h"
+
+
+
+//调试开关
+//#define  INIT_DEBUG
+
+#define _BV(bit) (1 << (bit))
+
+
+#define USART_MAX_DATA_SIZE		200
+
+
+#define SPIDER_CMD_TIME  800
+#define SPIDER_CMD1_TIME 800
+
+typedef struct 
+{
+  u8 CmdStartByte[2];
+  u8 Init_LED_ShakeFlag;				//初始化 ，ALL LED闪烁标志
+  u8 Init_LED_AlternateShakeFlag;		//初始化 ，LED交替闪烁标志
+  u8 Init_LED1_ShakeFlag;				//初始化 ，LED1闪烁标志
+
+}Main_STRUCT;
+
+
+extern Main_STRUCT  Main_StrPar;
+
+
+#endif
